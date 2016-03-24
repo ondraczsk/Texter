5be33f3dd55615a5f72640dab27e132d7d84f680
Jake <jakehcasto@aol.com>
2016-03-24 18:01:17 -0400

    Create Main.php

---
+++ src/Texter/Main.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Texter;
+
+use pocketmine\Player;
+use pocketmine\utils\TextFormat as Color;
+use pocketmine\command\{Command, CommandSender};
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\event\Listener;
+use pocketmine\level\particle\FloatingTextParticle;
+use pocketmine\level\particle\Particle;
+use pocketmine\level\Level;
+use pocketmine\level\Position;
+use pocketmine\level\Position\getLevel;
+use pocketmine\plugin\PluginManager;
+use pocketmine\plugin\Plugin;
+use pocketmine\math\Vector3;
+
+class Main extends PluginBase{
+    
+    public function onEnable(){
+        $this->getLogger()->info(Color::GREEN . "Enabled!");
+    }
+    
+    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+        if($cmd->getName() == "texter"){
+		$text = implode(" ", $args);
+		$position = new Vector3($sender->x, $sender->y + 0.5, $sender->z);
+        $sender->getLevel()->addParticle(new FloatingTextParticle($position, $text)); 
+        }
+        return true;
+    }
+}
